3f0d0e724bb407c01bb3a7e34a2254a10b567f5d
Jim <jimnilssonteit3@gmail.com>
2017-09-11 11:17:38 +0200

    Created headerfile for EntityManager.

---
+++ SluaghEngine/Core/include/EntityManager.h
@@ -0,0 +1,19 @@
+#ifndef SE_CORE_ENTITYMANAGER_H_
+#define SE_CORE_ENTITYMANAGER_H_
+
+namespace SE
+{
+	namespace Core
+	{
+		struct Entity
+		{
+			
+		};
+
+	}
+}
+
+
+
+#endif //SE_CORE_ENTITYMANAGER_H_
+
