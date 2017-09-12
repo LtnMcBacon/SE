e2a96a0510a963ef2390fe8e2fbf4cf71b12d39c
Jim <jimnilssonteit3@gmail.com>
2017-09-12 16:48:49 +0200

    Created core instance.

---
+++ SluaghEngine/Core/Engine.cpp
@@ -0,0 +1,23 @@
+#include "Core/Engine.h"
+#include "Core/EntityManager.h"
+
+SE::Core::Engine& SE::Core::Engine::GetInstance()
+{
+	static Engine instance;
+	return instance;
+}
+
+SE::Core::EntityManager& SE::Core::Engine::GetEntityManager()
+{
+	return *_entityManager;
+}
+
+SE::Core::Engine::Engine()
+{
+	_entityManager = new EntityManager;
+}
+
+SE::Core::Engine::~Engine()
+{
+	delete _entityManager;
+}
---
+++ SluaghEngine/includes/Core/Engine.h
@@ -0,0 +1,28 @@
+#ifndef SE_CORE_ENGINE_H_
+#define SE_CORE_ENGINE_H_
+namespace SE
+{
+	namespace Core
+	{
+		class EntityManager;
+		class Engine
+		{
+		public:
+			static Engine& GetInstance();
+
+			EntityManager& GetEntityManager();
+		private:
+			Engine();
+			Engine(const Engine& other) = delete;
+			Engine(const Engine&& other) = delete;
+			Engine& operator=(const Engine& rhs) = delete;
+			~Engine();
+
+			EntityManager* _entityManager;
+
+
+		};
+
+	}
+}
+#endif // SE_CORE_ENGINE_H_
