38bb5c180dca530eeecaeedfef7815bf9ac67fd5
Andreas Persson <leif.andreas.persson@gmail.com>
2017-09-21 12:11:55 +0200

    allocator code made test to go

--- SluaghEngine/Utilz/StackAllocator.cpp
+++ SluaghEngine/Utilz/StackAllocator.cpp
@@ -1,5 +1,6 @@
 #include "StackAllocator.h"
 #include <inttypes.h>
+#include <stdlib.h>
 namespace SE
 {
 	namespace Utilz
@@ -16,7 +17,7 @@ namespace SE
 
 		int StackAllocator::InitStackAlloc(size_t amountOfMemory)
 		{
-
+			stackBeg = malloc(amountOfMemory);
 		}
 
 		void* StackAllocator::GetMemoryAligned(size_t requestedMemSize, size_t alignment)
@@ -38,20 +39,44 @@ namespace SE
 			size_t expandSize_bytes = requestedMemSize + alignment;
 
 			//alloct unaligned, convert to uintptr_t
-			uintptr_t rawAddress = reinterpret_cast<uintptr_t>(GetMemUnaligned(expandSize_bytes));
-		
+			void* rawVoidptr = GetMemUnaligned(expandSize_bytes);
+			if (rawVoidptr = nullptr)
+			{
+				return nullptr;
+			}
+			uintptr_t rawAddress = reinterpret_cast<uintptr_t>(rawVoidptr);
+			
 			//determine how misaligned it is, (by masking of lower bits)
 			size_t mask = (alignment - 1);
 			uintptr_t misalignmnet = (rawAddress & mask);
+			ptrdiff_t adjustment = alignment - misalignmnet;
+
+			//calc adjusted address
+			uintptr_t alignedAddress = rawAddress + adjustment;
+
+			//store adjustment in byte before the aligned ptr
+			if (!(adjustment < 256))
+			{
+				return nullptr;
+			}
+			uint8_t* pAlignedMem = reinterpret_cast<uint8_t*>(alignedAddress);
+			pAlignedMem[-1] = static_cast<uint8_t>(adjustment);
+			
+			return static_cast<void*>(pAlignedMem);
 		}
 
 		void StackAllocator::ClearStackAlloc()
 		{
-
+			stackCurrent = stackBeg;
+			occupiedSize = 0;
 		}
 
 		void* StackAllocator::GetMemUnaligned(size_t requestedMemSize)
 		{
+			if (occupiedSize + requestedMemSize > stackSize)
+			{
+				return nullptr;
+			}
 			occupiedSize += requestedMemSize;
 			void* newMemPtr = stackCurrent;
 			stackCurrent = (char*)stackCurrent + requestedMemSize;
