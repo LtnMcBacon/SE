8522b41afdd43256dd8a3f396b3fcf54ef021e03
Jim <jimnilssonteit3@gmail.com>
2017-09-25 13:54:42 +0200

    Added some functon definitions in DebugRenderManager

--- SluaghEngine/Core/DebugRenderManager.cpp
+++ SluaghEngine/Core/DebugRenderManager.cpp
@@ -1,7 +1,7 @@
 #include "DebugRenderManager.h"
 
 SE::Core::DebugRenderManager::DebugRenderManager(Graphics::IRenderer* renderer, const EntityManager& entityManager,
-	TransformManager* transformManager) : entityManager(entityManager), transformManager(transformManager), renderer(renderer)
+	TransformManager* transformManager) : entityManager(entityManager), transformManager(transformManager), renderer(renderer), dirty(false), dynamicVertexBufferHandle(-1)
 {
 
 }
@@ -9,3 +9,52 @@ SE::Core::DebugRenderManager::DebugRenderManager(Graphics::IRenderer* renderer, 
 SE::Core::DebugRenderManager::~DebugRenderManager()
 {
 }
+
+void SE::Core::DebugRenderManager::Frame(Utilz::StackAllocator& perFrameStackAllocator)
+{
+	if(dirty)
+	{
+		size_t bufferSize = 0;
+		for (auto& m : entityToLineList)
+			bufferSize += m.second.size();
+		bufferSize *= sizeof(LineSegment);
+		void* lineData = perFrameStackAllocator.GetMemoryAligned(bufferSize, 4);
+		if (!lineData)
+			return;
+		for (auto& m : entityToLineList)
+		{
+			const size_t cpySize = m.second.size() * sizeof(LineSegment);
+			memcpy(lineData, m.second.data(), cpySize);
+			lineData = ((uint8_t*)lineData) + cpySize;
+			//Adding relevant functions in renderer before continuing
+		}
+		
+	}
+}
+
+void SE::Core::DebugRenderManager::ToggleDebugRendering(const Entity& entity, bool enable)
+{
+	if (!enable)
+		entityToLineList.erase(entity);
+}
+
+void SE::Core::DebugRenderManager::DrawCross(const Entity& entity, float scale, float x, float y, float z)
+{
+	const LineSegment lines[6] = { {{ x - scale, y, z }	,{ x + scale, y, z }},
+	{{ x, y, z - scale },{ x, y, z + scale } },
+	{{ x, y - scale, z },{ x, y + scale, z } } };
+	auto& lineList = entityToLineList[entity];
+	for (auto& p : lines)
+		lineList.push_back(p);
+	dirty = true;
+}
+
+void SE::Core::DebugRenderManager::DrawLine(const Entity& entity, float x1, float y1, float z1, float x2, float y2, float z2)
+{
+	const Point3D a = { x1,y1,z1 };
+	const Point3D b = { x2,y2,z2 };
+	const LineSegment l = { a, b };
+	entityToLineList[entity].push_back(l);
+	dirty = true;
+}
+
--- SluaghEngine/includes/Core/DebugRenderManager.h
+++ SluaghEngine/includes/Core/DebugRenderManager.h
@@ -4,11 +4,23 @@
 #include "EntityManager.h"
 #include "TransformManager.h"
 #include <Graphics/IRenderer.h>
+#include <Utilz/StackAllocator.h>
+
+#include <unordered_map>
+#include <vector>
 
 namespace SE
 {
 	namespace Core
 	{
+		/**
+		*
+		* @brief This manager is used to render debug-related elements using line lists. Lazy creation is employed which means that if an entity does not have a DebugRender-component when any of the methods in this class are called, the component will be created without having to explicitly create a component.
+		*
+		* @details Is dependent on a transform component being bound to the same entity for all methods.
+		*
+		**/
+
 		class DebugRenderManager
 		{
 		public:
@@ -19,11 +31,67 @@ namespace SE
 			DebugRenderManager& operator=(const DebugRenderManager& other) = delete;
 
 
+			/**
+			* @brief	Called each frame, to update the state.
+			*/
+			void Frame(Utilz::StackAllocator& perFrameStackAllocator);
+			/**
+			* @brief	Enable/disable debug rendering of the object. If the object has a bounding box it will also be rendered. Additional debug rendering can also be added to the entity such as crosses and lines.
+			* @param[in] entity Which entity.
+			* @param[in] enable True to enable, false to disable
+			*/
+			void ToggleDebugRendering(const Entity& entity, bool enable);
+
+			/**
+			* @brief	Draws a cross on the entity.
+			* @details The scale, x, y, and z parameters can be omitted and the cross will be drawn at the position and scale owned by the transform manager.
+			*			If the parameters are submitted they will be used in addition to the transform stored in the transform manager. For example if the
+			*			transform stored in the transform manager is {1, 1, 2} and the parameters {0, 2, 0} are supplied, the cross will be drawn at the
+			*			position {1, 3, 2}.
+			* @param[in] entity Which entity.
+			* @param[in] scale The size of the cross, defaults to 1.0f.
+			* @param[in] x The x-coordinate of the center of the cross.
+			* @param[in] y The y-coordinate of the center of the cross.
+			* @param[in] z The z-coordinate of the center of the cross.
+			*/
+			void DrawCross(const Entity& entity,float scale = 1.0f, float x = 0.0f, float y = 0.0f, float z = 0.0f);
+
+			/**
+			* @brief	Draws a line between two points.
+			* @details  The positions of the two points are given in local space. When rendered they will be transformed based on the transformation stored in the transform manager.
+			*			
+			* @param[in] entity Which entity.
+			* @param[in] x1 The x-coordinate of the the first point
+			* @param[in] y1 The y-coordinate of the the first point
+			* @param[in] z1 The z-coordinate of the the first point
+			* @param[in] x2 The x-coordinate of the the second point
+			* @param[in] y2 The y-coordinate of the the second point
+			* @param[in] z2 The z-coordinate of the the second point
+			*/
+			void DrawLine(const Entity& entity, float x1, float y1, float z1, float x2, float y2, float z2);
+
+
 		private:
+			struct Point3D
+			{
+				float x, y, z;
+			};
+			struct LineSegment
+			{
+				Point3D a;
+				Point3D b;
+			};
 			const EntityManager& entityManager;
 			TransformManager* transformManager;
-			
 			Graphics::IRenderer* renderer;
+
+			int dynamicVertexBufferHandle;
+			bool dirty;
+
+		
+
+
+			std::unordered_map<Entity, std::vector<LineSegment>> entityToLineList;
 
 		};
 	}
