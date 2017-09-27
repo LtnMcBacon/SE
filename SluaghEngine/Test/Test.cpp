c0da19a3a28ca295f852d20610814d72f7534ae1
Jim <jimnilssonteit3@gmail.com>
2017-09-27 16:05:39 +0200

    Removed a breakpoint.

--- SluaghEngine/Test/Test.cpp
+++ SluaghEngine/Test/Test.cpp
@@ -44,7 +44,7 @@ int main(int argc, char** argv)
 	srand(time(NULL));
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-	_crtBreakAlloc = 1777261;
+	//_crtBreakAlloc = 1777261;
 
 	//std::map<SE::Utilz::GUID, std::tuple<const char*,Test*>, SE::Utilz::GUID::Compare> tests;
 	AddTest(EntityManagerTest);
