e22b81fe23478efe2c7a5687b80c7859fcd0c787
Andreas Jonasson <andreas.jonasson1993@gmail.com>
2017-10-03 14:29:46 +0200

    Made sure that every function has the profiler included.

--- SluaghEngine/Gameplay/Succeeder.cpp
+++ SluaghEngine/Gameplay/Succeeder.cpp
@@ -1,12 +1,14 @@
 #include "Succeeder.h"
+#include <Profiler.h>
 
 using namespace SE;
 using namespace GAMEPLAY;
 
 Status Succeeder::Update()
 {
+	StartProfile
 	myChild->Tick();
-	return myStatus;
+	ProfileReturn(myStatus);
 }
 
 Succeeder::Succeeder(EnemyBlackboard* enemyBlackboard, GameBlackboard* gameBlackboard, IBehaviour* child) :
