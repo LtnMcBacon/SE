6b03fa16bf1966393a57c93ba1aa9b5e8a85faf4
Andreas Persson <leif.andreas.persson@gmail.com>
2017-10-05 11:15:10 +0200

    changed timer to use template for chrona type and moved all to the .h file

--- SluaghEngine/includes/Utilz/Timer.h
+++ SluaghEngine/includes/Utilz/Timer.h
@@ -14,20 +14,69 @@ namespace SE
 
 
 		public:
-			Timer();
-			~Timer();
-			void Tick();
-			float GetDeltaSeconds() const;
-			float GetDeltaMilliseconds() const;
+			Timer()
+			{
+				curTime = std::chrono::high_resolution_clock::now();
+				prevTime = curTime;
+			};
+			~Timer() {};
+			void Tick()
+			{
+				prevTime = curTime;
+				curTime = std::chrono::high_resolution_clock::now();
+				int index = 0;
+				while (index < delegateVec.size())
+				{
+					if (curTime - delegateVec[index].startTime > delegateVec[index].delayTime)
+					{
+						delegateVec[index].delegateCall();
+						delegateVec.erase(delegateVec.begin() + index);
+						continue;
+					}
+					index++;
+				}
+			};
 
-			int StartStopWatch();
-			float StopStopWatch();
+			template<typename TimeType = std::chrono::milliseconds>
+			inline float GetDelta() const
+			{
+				return (float)std::chrono::duration_cast<TimeType>(curTime - prevTime).count();
+			};
+
+			inline int StartStopWatch()
+			{
+				if (watchState == false)
+				{
+					watchStart = std::chrono::high_resolution_clock::now();
+					watchState = true;
+					return 0;
+				}
+				return -1;
+			};
+
+			template<typename TimeType = std::chrono::milliseconds>
+			inline float StopStopWatch()
+			{
+				if (watchState == true)
+				{
+					watchState = false;
+					return (float)std::chrono::duration_cast<TimeType>(std::chrono::high_resolution_clock::now() - watchStart).count();
+				}
+				return 0.0f;
+			};
 
 			/**
 			* @brief Binds a callback function that is called after a certain amount of time.
 			* @sa Delegate
 			*/
-			void BindAfterTimeCallback(const AfterTimeCallback& callback, std::chrono::seconds time);
+			void BindAfterTimeCallback(const AfterTimeCallback& callback, std::chrono::seconds time)
+			{
+				delegateTime tempDT;
+				tempDT.delegateCall = callback;
+				tempDT.delayTime = time;
+				tempDT.startTime = std::chrono::high_resolution_clock::now();
+				delegateVec.push_back(tempDT);
+			};
 			
 		private:
 			std::chrono::high_resolution_clock::time_point curTime;
