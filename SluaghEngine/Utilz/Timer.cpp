a7a05541f5151a2a0dabb6e6783dd094ed3aeb8f
Andreas Persson <leif.andreas.persson@gmail.com>
2017-10-05 10:36:27 +0200

    added stopwatch and delay delegate

--- SluaghEngine/Utilz/Timer.cpp
+++ SluaghEngine/Utilz/Timer.cpp
@@ -16,6 +16,17 @@ void SE::Utilz::Timer::Tick()
 {
 	prevTime = curTime;
 	curTime = std::chrono::high_resolution_clock::now();
+	int index = 0;
+	while (index < delegateVec.size())
+	{
+		if (curTime - delegateVec[index].startTime > delegateVec[index].delayTime)
+		{
+			delegateVec[index].delegateCall();
+			delegateVec.erase(delegateVec.begin + index);
+			continue;
+		}
+		index++;
+	}
 }
 
 float SE::Utilz::Timer::GetDeltaSeconds() const
@@ -26,4 +37,34 @@ float SE::Utilz::Timer::GetDeltaSeconds() const
 float SE::Utilz::Timer::GetDeltaMilliseconds() const
 {
 	return (float)std::chrono::duration_cast<std::chrono::milliseconds>(curTime - prevTime).count();
+}
+
+int SE::Utilz::Timer::StartStopWatch()
+{
+	if (watchState == false)
+	{
+		watchStart = std::chrono::high_resolution_clock::now();
+		watchState = true;
+		return 0;
+	}
+	return -1;
+}
+
+float SE::Utilz::Timer::StopStopWatch()
+{
+	if (watchState == true)
+	{
+		watchState = false;
+		return (float)std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - watchStart).count;
+	}
+	return 0.0f;
+}
+
+void SE::Utilz::Timer::BindAfterTimeCallback(const AfterTimeCallback & callback, std::chrono::seconds time)
+{
+	delegateTime tempDT;
+	tempDT.delegateCall = callback;
+	tempDT.delayTime = time;
+	tempDT.startTime = std::chrono::high_resolution_clock::now();
+	delegateVec.push_back(tempDT);
 }
--- SluaghEngine/includes/Utilz/Timer.h
+++ SluaghEngine/includes/Utilz/Timer.h
@@ -2,10 +2,13 @@
 #include <windows.h>
 #include <time.h>
 #include <chrono>
+#include <vector>
+#include <Utilz/Delegate.h>
 namespace SE
 {
 	namespace Utilz
 	{
+		typedef Utilz::Delegate<void()> AfterTimeCallback;
 		class Timer
 		{
 
@@ -16,11 +19,32 @@ namespace SE
 			void Tick();
 			float GetDeltaSeconds() const;
 			float GetDeltaMilliseconds() const;
+
+			int StartStopWatch();
+			float StopStopWatch();
+
+			/**
+			* @brief Binds a callback function that is called after a certain amount of time.
+			* @sa Delegate
+			*/
+			void BindAfterTimeCallback(const AfterTimeCallback& callback, std::chrono::seconds time);
 			
 		private:
 			std::chrono::high_resolution_clock::time_point curTime;
 			std::chrono::high_resolution_clock::time_point prevTime;
 
+			// stopwatch
+			std::chrono::high_resolution_clock::time_point watchStart;
+			bool watchState = false;
+
+			// delegate delay
+			struct delegateTime
+			{
+				AfterTimeCallback delegateCall;
+				std::chrono::seconds delayTime;
+				std::chrono::high_resolution_clock::time_point startTime;
+			};
+			std::vector<delegateTime> delegateVec;
 
 		};
 	}
