b65202940e8818d8ee8edcd2017fed04a9ba788a
Peter <dastykill@gmail.com>
2017-10-10 11:16:20 +0200

    TimerCluster

--- SluaghEngine/Utilz/TimeCluster.h
+++ SluaghEngine/Utilz/TimeCluster.h
@@ -1,7 +1,8 @@
 #ifndef SE_UTILZ_TIME_CLUSTER_H_
 #define SE_UTILZ_TIME_CLUSTER_H_
-#include <Timer.h>
 #include <GUID.h>
+#include <map>
+#include <chrono>
 namespace SE
 {
 	namespace Utilz
@@ -9,9 +10,29 @@ namespace SE
 		class TimeCluster
 		{
 		public:
-			void Start(const GUID&)
+			void Start(const GUID& id)
+			{
+				timers[id].startTime = std::chrono::high_resolution_clock::now();
+			}
+			void Stop(const GUID& id)
+			{
+				timers[id].stopTime = std::chrono::high_resolution_clock::now();
+			}
+
+			template<typename TimeType = std::chrono::milliseconds>
+			float GetTime(const GUID& id)
+			{
+				return static_cast<float>(std::chrono::duration_cast<TimeType>(timers[id].stopTime - timers[id].startTime).count());
+			}
 		private:
-			
+			struct Timer
+			{
+				std::chrono::high_resolution_clock::time_point startTime;
+				std::chrono::high_resolution_clock::time_point stopTime;
+				Timer() : startTime(std::chrono::high_resolution_clock::now()), stopTime(std::chrono::high_resolution_clock::now()) {};
+			};
+
+			std::map<GUID, Timer, GUID::Compare> timers;
 					
 		};
 	}
