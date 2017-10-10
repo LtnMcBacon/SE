8b96198ae52fa71cff4ffdee84349eaa53e65b04
Peter <dastykill@gmail.com>
2017-10-10 11:05:25 +0200

    TimeCluster

---
+++ SluaghEngine/Utilz/TimeCluster.h
@@ -0,0 +1,21 @@
+#ifndef SE_UTILZ_TIME_CLUSTER_H_
+#define SE_UTILZ_TIME_CLUSTER_H_
+#include <Timer.h>
+#include <GUID.h>
+namespace SE
+{
+	namespace Utilz
+	{
+		class TimeCluster
+		{
+		public:
+			void Start(const GUID&)
+		private:
+			
+					
+		};
+	}
+}
+
+
+#endif //SE_UTILZ_TIME_CLUSTER_H_
