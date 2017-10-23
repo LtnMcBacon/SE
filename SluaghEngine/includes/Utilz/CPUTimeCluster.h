9fbc85a3924ed4bd2755fc4e57809a51a933a462
Peter <dastykill@gmail.com>
2017-10-23 14:17:49 +0200

    frametimes

--- SluaghEngine/includes/Utilz/CPUTimeCluster.h
+++ SluaghEngine/includes/Utilz/CPUTimeCluster.h
@@ -9,17 +9,26 @@ namespace SE
 		class CPUTimeCluster : public TimeCluster
 		{
 		public:
-			CPUTimeCluster() : TimeCluster() {};
+			CPUTimeCluster() : TimeCluster(), currentFrame(0){};
 			~CPUTimeCluster() {};
 
 
 			inline void Start(const Utilz::IDHash& id)override
 			{
-				timers[id].startTime = std::chrono::high_resolution_clock::now();
+				if (!timers[id].Started && !timers[id].Finished)
+				{
+					timers[id].startTime[currentFrame] = std::chrono::high_resolution_clock::now();
+					timers[id].Started = true;
+				}
 			};
 			inline void Stop(const Utilz::IDHash& id)override
 			{
-				timers[id].stopTime = std::chrono::high_resolution_clock::now();
+				if (timers[id].Started && !timers[id].Finished)
+				{
+					timers[id].stopTime[currentFrame] = std::chrono::high_resolution_clock::now();
+					timers[id].Started = false;
+					timers[id].Finished = true;
+				}
 			}
 
 			/**
@@ -29,21 +38,32 @@ namespace SE
 			template<typename Ratio = std::milli>
 			inline float GetTime(const Utilz::IDHash& id)
 			{
-				return std::chrono::duration<float, Ratio>(timers[id].stopTime - timers[id].startTime).count();
+				if (timers[id].Finished)
+				{
+					timers[id].Finished = false;
+					timers[id].time[currentFrame] = std::chrono::duration<float, Ratio>(timers[id].stopTime[currentFrame] - timers[id].startTime[currentFrame]).count();
+					
+				}
+				return timers[id].time[currentFrame];
 			}
 
 
 			inline void GetMap(TimeMap& map)override
 			{
+				currentFrame = (currentFrame + 1) % frameDelay;
 				for (auto& timer : timers)
 					map[timer.first] = this->GetTime(timer.first);
 			}
 		private:
+			static const uint8_t frameDelay = 2;
+			uint8_t currentFrame;
 			struct Timer
 			{
-				std::chrono::high_resolution_clock::time_point startTime;
-				std::chrono::high_resolution_clock::time_point stopTime;
-				Timer() : startTime(std::chrono::high_resolution_clock::now()), stopTime(std::chrono::high_resolution_clock::now()) {};
+				bool Started = false;
+				bool Finished = false;
+				float time[frameDelay] = { 0.0f ,0.0f };
+				std::chrono::high_resolution_clock::time_point startTime[frameDelay];
+				std::chrono::high_resolution_clock::time_point stopTime[frameDelay];
 			};
 
 			std::map<Utilz::IDHash, Timer, Utilz::IDHash::Compare> timers;
