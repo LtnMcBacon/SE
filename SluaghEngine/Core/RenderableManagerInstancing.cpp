049801f9cb0cb873f6f8a826b5445990b6eeff31
Peter <dastykill@gmail.com>
2017-10-25 15:46:26 +0200

    Adding entity if a fitting buckets exists.

--- SluaghEngine/Core/RenderableManagerInstancing.cpp
+++ SluaghEngine/Core/RenderableManagerInstancing.cpp
@@ -13,4 +13,17 @@ SE::Core::RenderableManagerInstancing::~RenderableManagerInstancing()
 
 void SE::Core::RenderableManagerInstancing::AddEntity(const Entity & entity, const Graphics::RenderJob & job)
 {
+	const auto findBucketAndID = entityToRenderBucketAndID.find(entity);
+	if (findBucketAndID == entityToRenderBucketAndID.end()) // Entity is not in any bucket.
+	{
+		for (auto& bucket : renderBuckets)
+		{
+			if (bucket.pipeline == job.pipeline) // The job fits in an already existing bucket.
+			{
+				
+			}
+		}
+
+
+	}
 }
