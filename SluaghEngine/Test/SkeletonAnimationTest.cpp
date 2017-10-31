0b543d61ba5232d50a6e2b25b7c5d47e3dbed604
Fredrik Linde <gearfreak@hotmail.se>
2017-10-31 11:29:38 +0100

    Changed light in skeleton animation test

--- SluaghEngine/Test/SkeletonAnimationTest.cpp
+++ SluaghEngine/Test/SkeletonAnimationTest.cpp
@@ -129,7 +129,7 @@ bool SE::Test::SkeletonAnimationTest::Run(DevConsole::IConsole * console)
 	auto& l = managers.entityManager->Create();
 	Core::ILightManager::CreateInfo d;
 	d.radius = 100.0f;
-	d.pos = { 0.0f, 5.0f, -5.0f };
+	d.pos = { 0.0f, 0.0f, -5.0f };
 	d.color = { 1, 1,1 };
 	d.castShadow = false;
 	d.isOrtographic = false;
