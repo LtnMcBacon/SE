22174850e666325123f8178fba5e1c4b8f83a6cc
Philip Velandria <philipvelandria@gmail.com>
2017-11-13 12:47:32 +0100

    perk importer class

---
+++ SluaghEngine/Gameplay/PerkImporter.cpp
@@ -0,0 +1,9 @@
+#include "PerkImporter.h"
+
+SE::Gameplay::PerkImporter::PerkImporter()
+{
+}
+
+SE::Gameplay::PerkImporter::~PerkImporter()
+{
+}
---
+++ SluaghEngine/Gameplay/PerkImporter.h
@@ -0,0 +1,22 @@
+#ifndef _SE_GAMEPLAY_PERKIMPORTER_H_
+#define _SE_GAMEPLAY_PERKIMPORTER_H_
+
+namespace SE
+{
+	namespace Gameplay
+	{
+		class PerkImporter
+		{
+		public:
+			PerkImporter();
+			~PerkImporter();
+
+		private:
+
+		};
+	}
+
+}
+
+
+#endif
