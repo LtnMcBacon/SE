f62e805957de0c1fc71c9edb65701b559e91dff1
Andreas Persson <leif.andreas.persson@gmail.com>
2017-11-22 13:12:00 +0100

    fixed so partSys garbage collection depends on amount of systems. Maby change later to a constant thats higher than 4 depending on how many systems we will be using.

--- SluaghEngine/Core/ParticleSystemManager.cpp
+++ SluaghEngine/Core/ParticleSystemManager.cpp
@@ -1,6 +1,9 @@
 #include "ParticleSystemManager.h"
 #include <Profiler.h>
+#include <algorithm>
 
+#undef min
+#undef max
 
 SE::Core::ParticleSystemManager::ParticleSystemManager(const InitializationInfo& initInfo) : initInfo(initInfo)
 {
@@ -420,7 +423,8 @@ void SE::Core::ParticleSystemManager::GarbageCollection()
 {
 	StartProfile;
 	uint8_t alive_in_row = 0;
-	while (particleSystemData.size() > 0 && alive_in_row < 4U)
+	const uint32_t quitWhenReached = std::max((uint32_t)(particleSystemData.size() * 0.02f), 40U);
+	while (particleSystemData.size() > 0 && alive_in_row < quitWhenReached)
 	{
 		std::uniform_int_distribution<size_t> distribution(0U, particleSystemData.size() - 1U);
 		size_t i = distribution(generator);
