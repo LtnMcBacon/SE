7d0f41ae54425e2613029e192503dff2d8d3a338
Jim <jimnilssonteit3@gmail.com>
2017-11-29 10:56:03 +0100

    Added some flavor to the winstate.

--- SluaghEngine/Gameplay/WinState.cpp
+++ SluaghEngine/Gameplay/WinState.cpp
@@ -13,17 +13,43 @@ SE::Gameplay::WinState::WinState()
 	camc.aspectRatio = sys.optionsHandler->GetOptionDouble("Camera", "aspectRatio", 1280.0f / 720.0f);
 	man.cameraManager->Create(cam, camc);
 	man.cameraManager->SetActive(cam);
-	man.transformManager->SetPosition(cam, { 0.0f, 0.0f, -5.0f });
+	man.transformManager->SetPosition(cam, { 0.0f, 0.0f, 0.0f });
 
 	deadSluagh = man.entityManager->Create();
+	man.transformManager->Create(deadSluagh, { 0.0f, 0.0f, 2.0f }, {0.0f, 3.14f, 0.0f});
+	Core::IMaterialManager::CreateInfo info;
+	auto shader = Utilz::GUID("SimpleLightPS.hlsl");
+	auto material = Utilz::GUID("Slaughplane.mat");
+	info.shader = shader;
+	info.materialFile = material;
+	man.materialManager->Create(deadSluagh, info);
+
 	Core::IAnimationManager::CreateInfo deadcinfo;
 	deadcinfo.mesh = "MCModell.mesh";
 	deadcinfo.skeleton = "MCModell.skel";
 	Utilz::GUID anims[] = { "DeathAnim_MCModell.anim" };
 	deadcinfo.animations = anims;
 	man.animationManager->CreateAnimatedObject(deadSluagh, deadcinfo);
-	man.animationManager->Start(deadSluagh, anims, 1, 1.0f, Core::AnimationFlags::IMMEDIATE);
+	man.animationManager->Start(deadSluagh, anims, 1, 2.0f, Core::AnimationFlags::IMMEDIATE);
 	man.animationManager->ToggleVisible(deadSluagh, true);
+
+	sluaghSoul = man.entityManager->Create();
+	man.transformManager->Create(sluaghSoul, { 0.0f, 0.0f, 2.0f }, { 0.0f, 3.14f, 0.0f });
+	Core::IMaterialManager::CreateInfo info2;
+	auto shader2 = Utilz::GUID("SimpleLightTransparentPS.hlsl");
+	auto material2 = Utilz::GUID("Slaughplane.mat");
+	info.shader = shader2;
+	info.materialFile = material2;
+	man.materialManager->Create(sluaghSoul, info);
+
+	Core::IAnimationManager::CreateInfo deadcinfo2;
+	deadcinfo2.mesh = "MCModell.mesh";
+	deadcinfo2.skeleton = "MCModell.skel";
+	Utilz::GUID anims2[] = { "DeathAnim_MCModell.anim" };
+	deadcinfo2.animations = anims2;
+	man.animationManager->CreateAnimatedObject(sluaghSoul, deadcinfo2);
+	man.animationManager->Start(sluaghSoul, anims2, 1, 2.0f, Core::AnimationFlags::IMMEDIATE);
+	
 
 	auto l = man.entityManager->Create();
 	Core::ILightManager::CreateInfo d;
@@ -43,6 +69,9 @@ SE::Gameplay::WinState::WinState()
 	man.textManager->ToggleRenderableText(victoryText, true);
 
 	sys.window->MapActionButton(Window::KeyReturn, Window::KeyReturn);
+	stateToReturn = State::WIN_STATE;
+	timer.Tick();
+	totTime = 0.0f;
 }
 
 SE::Gameplay::WinState::~WinState()
@@ -52,8 +81,15 @@ SE::Gameplay::WinState::~WinState()
 
 SE::Gameplay::IGameState::State SE::Gameplay::WinState::Update(void*&)
 {
-
+	timer.Tick();
+	float dt = timer.GetDelta<std::ratio<1,1>>();
+	totTime += dt;
+	if(totTime > 2.0f)
+	{
+		man.animationManager->ToggleVisible(sluaghSoul, true);
+		man.transformManager->Move(sluaghSoul, DirectX::XMFLOAT3(0.0f, 0.5f * dt, 0.0f));
+	}
 	if (sys.window->ButtonPressed(Window::KeyReturn))
 		return State::CHARACTER_CREATION_STATE;
-	return IGameState::State::WIN_STATE;
+	return stateToReturn;
 }
--- SluaghEngine/includes/Gameplay/WinState.h
+++ SluaghEngine/includes/Gameplay/WinState.h
@@ -1,6 +1,7 @@
 #ifndef SE_GAMEPLAY_WIN_STATE_H_
 #define SE_GAMEPLAY_WIN_STATE_H_
 #include "IGameState.h"
+#include "Utilz/Timer.h"
 
 namespace SE::Gameplay
 {
@@ -15,10 +16,14 @@ namespace SE::Gameplay
 	private:
 		Core::Entity cam;
 		Core::Entity deadSluagh;
+		Core::Entity sluaghSoul;
 		Core::Entity victoriousScot;
 		Core::Entity victoryText;
 		Core::Entity quitButton;
 		Core::Entity newCharacterButton;
+		Utilz::Timer timer;
+		State stateToReturn;
+		float totTime;
 	};
 }
 
