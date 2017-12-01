63c9f7747b2fada2e4bc008a60675e9a91ba8743
Jonah <jonahnapieralla@gmail.com>
2017-12-01 15:41:52 +0100

    Wrote code for center of screen fog attraction.

--- SluaghEngine/Build/Asset/Shaders/Pixel/FogTopPS.hlsl
+++ SluaghEngine/Build/Asset/Shaders/Pixel/FogTopPS.hlsl
@@ -1,5 +1,9 @@
-static const float fogSize = 1;
-static const float cycleTime = 12;
+static const float FOG_SIZE = 1;
+static const float CYCLE_TIME = 12;
+
+//static const uint2 WINDOW_RESOLUTION = { 1280, 720 };
+//static const float VIEW_ROTATION = 55;
+//static const float CENTER_ATTRACTION_STRENGTH = 1;
 
 
 Texture2D albedoTexture : register(t0);
@@ -14,8 +18,12 @@ cbuffer timeBuffer : register(b0)
 };
 
 
-static const float fogSpeed = 2.5 / cycleTime;
-static const float cycleTime_quarter = cycleTime / 4;
+static const float2 ROOM_CENTER = float2(12.5, 12.5);
+static const float FOG_SPEED = 2.5 / CYCLE_TIME;
+static const float CYCLE_TIME_QUARTER = CYCLE_TIME / 4;
+//static const float2 WINDOW_RESOLUTION_HALF = { WINDOW_RESOLUTION.x / 2.f, WINDOW_RESOLUTION.y / 2.f };
+//static const float VIEW_ROTATION_INVERSE_SINE = sin(-(3.1416 / 180 * VIEW_ROTATION));
+//static const float VIEW_ROTATION_INVERSE_COSINE = cos(-(3.1416 / 180 * VIEW_ROTATION));
 
 
 struct PS_IN
@@ -28,22 +36,29 @@ struct PS_IN
 
 float4 PS_main(PS_IN input) : SV_TARGET
 {
-    float clock = time % cycleTime;
+    float clock = time % CYCLE_TIME;
 
 
-    float2 uvOffset = input.uv - float2(12.5, 12.5);
+    float2 uvOffset = input.uv - ROOM_CENTER;
     float2 uvOffset_signs = float2(sign(uvOffset.x), sign(uvOffset.y));
 
     uvOffset = normalize(uvOffset);
     uvOffset *= uvOffset;
     uvOffset *= uvOffset_signs;
 
-    uvOffset *= clock;
-    uvOffset *= fogSpeed;
+
+    //float2 centerOffset = { input.position.x - WINDOW_RESOLUTION_HALF.x, (WINDOW_RESOLUTION.y - input.position.y) - WINDOW_RESOLUTION_HALF.y };
+    //centerOffset = normalize(centerOffset);
+
+    //centerOffset.x = VIEW_ROTATION_INVERSE_COSINE * centerOffset.x - VIEW_ROTATION_INVERSE_SINE * centerOffset.y;
+    //centerOffset.y = VIEW_ROTATION_INVERSE_SINE * centerOffset.x + VIEW_ROTATION_INVERSE_COSINE * centerOffset.y;
+
+    //uvOffset += CENTER_ATTRACTION_STRENGTH * centerOffset;
+    uvOffset *= FOG_SPEED * clock;
 
 
-    float4 albedoValues = albedoTexture.Sample(samplerState, (input.uv + uvOffset) / fogSize);
-    float4 normalValues = normalTexture.Sample(samplerState, (input.uv + uvOffset) / fogSize);
+    float4 albedoValues = albedoTexture.Sample(samplerState, (input.uv + uvOffset) / FOG_SIZE);
+    float4 normalValues = normalTexture.Sample(samplerState, (input.uv + uvOffset) / FOG_SIZE);
 
 
     float3 albedoColor = albedoValues.rgb;
@@ -54,7 +69,7 @@ float4 PS_main(PS_IN input) : SV_TARGET
     float diffuseFactor = dot(float3(0, 0, 1), normalColor);
 
 
-    float swapMultiplier = clock / cycleTime_quarter;
+    float swapMultiplier = clock / CYCLE_TIME_QUARTER;
 
     float sm_fade = max(0, swapMultiplier - 2);
     bool sm_fade_active = sm_fade;
