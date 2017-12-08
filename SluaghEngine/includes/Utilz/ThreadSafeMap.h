ee4a87df4c500eadcaef700d2eb697e1a52766ba
Jim <jimnilssonteit3@gmail.com>
2017-12-08 10:14:41 +0100

    Changed threadsafemap to allow multiple readers single writer.

--- SluaghEngine/includes/Utilz/ThreadSafeMap.h
+++ SluaghEngine/includes/Utilz/ThreadSafeMap.h
@@ -1,30 +1,28 @@
 #ifndef SE_UTILZ_THREADSAFE_MAP_H
 #define SE_UTILZ_THREADSAFE_MAP_H
-#include <mutex>
+#include <shared_mutex>
 #include <unordered_map>
 namespace SE
 {
 	namespace Utilz
 	{
 
-
+		/*
+		 * @brief An unordered map wrapped with a mutex lock.
+		 */
 		template <typename Key, typename Value, typename Hasher>
 		class ThreadSafeMap
 		{
 		public:
 			Value& operator[](const Key& key)
 			{
-				lock.lock();
-				auto& entry = umap[key];
-				lock.unlock();
-				return entry;
+				std::unique_lock<std::shared_mutex> lock(mutex);
+				return umap[key];
 			}
 			typename std::unordered_map<Key, Value, Hasher>::iterator find(const Key& key)
 			{
-				lock.lock();
-				auto it = umap.find(key);
-				lock.unlock();
-				return it;
+				std::shared_lock<std::shared_mutex> lock(mutex);
+				return umap.find(key);
 			}
 
 			typename std::unordered_map<Key, Value, Hasher>::iterator end()
@@ -40,18 +38,16 @@ namespace SE
 
 			void erase(typename std::unordered_map<Key, Value, Hasher>::iterator it)
 			{
-				lock.lock();
+				std::unique_lock<std::shared_mutex> lock(mutex);
 				umap.erase(it);
-				lock.unlock();
 			}
 			void erase(const Key& key)
 			{
-				lock.lock();
+				std::unique_lock<std::shared_mutex> lock(mutex);
 				umap.erase(key);
-				lock.unlock();
 			}
 		private:
-			std::mutex lock;
+			std::shared_mutex mutex;
 			std::unordered_map<Key, Value, Hasher> umap;
 		};
 	}
