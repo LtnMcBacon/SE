ca1f2e2e537c7f3aca3cdc633e89175882985c97
thecarrera <the_carrera@hotmail.com>
2017-12-14 08:44:49 +0100

    quick fix

--- SluaghEngine/includes/Gameplay/Stats.h
+++ SluaghEngine/includes/Gameplay/Stats.h
@@ -24,7 +24,7 @@ namespace SE
 			float damage = 1.f;
 			static const int MAX_DAMAGE = 10;
 			float meleeMultiplier = 1.f;
-			float physicalResistance = -1.f;
+			float physicalResistance = 0.f;
 
 			//agi
 			float rangedDamage = 1.f;
@@ -36,10 +36,10 @@ namespace SE
 			//whi
 			float magicDamage = 1.f;
 			float magicMultiplier = 1.f;
-			float magicResistance = -1.f;
-			float natureResistance = -1.f;
-			float fireResistance = -1.f;
-			float waterResistance = -1.f;
+			float magicResistance = 0.f;
+			float natureResistance = 0.f;
+			float fireResistance = 0.f;
+			float waterResistance = 0.f;
 
 			ArmourType armour = ArmourType::ARMOUR_TYPE_NONE;
 			DamageSources weapon = DamageSources::DAMAGE_SOURCE_MELEE;
